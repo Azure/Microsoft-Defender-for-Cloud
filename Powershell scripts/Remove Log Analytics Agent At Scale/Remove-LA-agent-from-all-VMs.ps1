90d5c663462e0e0dfeb2cdc9fba389c95aedb368
Liana <tomescul@tcd.ie>
2022-11-02 23:08:46 +0000

    Uploaded Powershell file to remove LA agent

---
+++ Powershell scripts/Remove Log Analytics Agent At Scale/Remove-LA-agent-from-all-VMs.ps1
@@ -0,0 +1,6 @@
+﻿$VMs = Get-AzVM $WindowsVMs = $VMs | Where-Object { $PSItem.StorageProfile.ImageReference.Offer -eq "WindowsServer" }
+foreach ($VM in $WindowsVMs) {    $extension = Get-AzVMExtension -ResourceGroupName $Vm.ResourceGroupName -VMName $VM.Name
+if ($extension.Name -contains "MicrosoftMonitoringAgent") {        Write-Host "Microsoft Monitoring Agent is Installed on" $VM.Name
+Start-AzVM -ResourceGroupName $Vm.ResourceGroupName -Name $VM.Name
+Remove-AzVMExtension -ResourceGroupName $Vm.ResourceGroupName -Name "MicrosoftMonitoringAgent" -VMName $VM.Name
+}    }
