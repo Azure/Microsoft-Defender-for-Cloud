5583b258957269cb957afa7450cbc39ee5d98a49
swiftsolves-msft <naswif@microsoft.com>
2023-01-20 13:33:22 -0500

    a couple scripts around mdc-mde onboard
    2 ps scripts
    
    1. onboard a single az vm onto mde. vm extension using the API
    
    2. generate report of mde. vm extension failures across azure subscriptions to help identify patterns.

---
+++ Powershell scripts/Enterprise Report on MDC - MDE VM Extension Failures/MDEExtErrorReport.ps1
@@ -0,0 +1,63 @@
+﻿#---------------------------------------------------------------------------------------------------
+# Script to generate a .csv report of failed MDE. VM Extensions, useful for filtering and grouping
+#---------------------------------------------------------------------------------------------------
+
+# Outputfile for MDE extension error report
+
+$path = "C:\temp\mdeextreport.txt"
+$csvpath = "C:\temp\mdeextreport.csv"
+$outputFile = $path
+
+#Set and apply 1st line of csv headers
+$string = "VM Name,VM Extension,VM Extension Status,VM Extension Error,VM Extension Detailed Errors"
+$string | Out-File $outputFile -append -force
+
+# get all subscriptions
+$Subs = Get-AzSubscription
+
+
+# for each subscription get all the VMs and their status
+foreach($sub in $subs){
+
+    # set a particular subscription context to search for VMs
+    Set-AzContext -Subscription $Sub.Id
+
+    # get all the VMs in Subscription including status
+    $vms = Get-AzVM
+
+    #For each VM check for MDE.windows or MDE.Linux extension and if in a error state record information to csv
+    foreach($vm in $vms){
+
+        #get particular extension details of VM
+        $vm = Get-AzVm -ResourceGroupName $vm.ResourceGroupName  -Name $vm.Name -Status
+
+        # conditional check for VM if it has MDE extension
+        if($vm.Extensions.Name -match "MDE"){
+
+            # set variable for status level of MDE extension
+            $level = ($vm.Extensions | where-Object {($_.Name -match "MDE")}).Statuses.level
+
+            # if the mde extension is not successful then generate a entry in the report for the vm and informnationa round mde extension
+            if($level -ne "Info"){
+            
+                # future build for sub status detailed errors, is in a list, would need to foreach and join into a single string with a unique delimiter
+                $SubMessages = ($vm.Extensions | where-Object {($_.Name -match "MDE")}).Substatuses.message
+
+                #generate entry for report
+                $string = "$($vm.Name), $(($vm.Extensions | where-Object {($_.Name -match "MDE")}).Name), $(($vm.Extensions | where-Object {($_.Name -match "MDE")}).Statuses.level), $(($vm.Extensions | where-Object {($_.Name -match "MDE")}).Statuses.Message)"               
+                
+                #output entry into report
+                $string | Out-File $outputFile -append -force
+            
+            }
+        
+        }    
+    
+    }
+
+}
+
+# Once done import the data into excel
+
+$CSV = Import-Csv -Path $path
+$CSV | Export-Csv -Path $csvpath -NoTypeInformation
---
+++ Powershell scripts/MDE VM Extension Single VM Onboard/MDESingleOnboard.ps1
@@ -0,0 +1,28 @@
+﻿#---------------------------------------------------------------------------------------------
+# Script to Obtain MDE. VM Extension Base64Package and Deploy MDE VM Extension on a Single VM
+#---------------------------------------------------------------------------------------------
+
+#PRE REQs - ARMCLient, Azure CLI, Azure PowerShell
+
+# Login in
+Connect-AzAccount
+armclient login
+
+#Your AAD Tenant ID if your login is associated to many tenants
+armclient token {YOUR AAD TENANT ID}
+
+# URL to get the base 64 encoded package
+$url1 = "https://management.azure.com/subscriptions/{SUBID}/providers/Microsoft.Security/mdeOnboardings?api-version=2021-10-01-preview"
+
+
+# PUT Api to setup continuous export rule to send to Storage
+armclient GET $url1
+
+# URL and payload for MDE Onboard for a Single Windows Azure VM
+$url2 = "https://management.azure.com/{RESOURCEID}/extensions/MDE.Windows?api-version=2015-06-15"
+
+$payload2 = "{'name': 'MDE.Windows', 'id': '{RESOURCEID}/extensions/MDE.Windows', 'type': 'Microsoft.compute/virtualMachines/extensions', 'location': '{VMLOCATION}', 'properties': {'autoUpgradeMinorVersion': true, 'publisher': 'Microsoft.Azure.AzureDefenderForServers','type': 'MDE.Windows','typeHandlerVersion': '1.0','settings': {'azureResourceId': '{RESOURCEID}','vNextEnabled': 'true'},'protectedSettings': {'defenderForEndpointOnboardingScript': '{BASE64PACKAGE}'}}}"
+
+
+# PUT Api to setup continuous export rule to send to Event Hubs
+armclient PUT $url2 $payload2
