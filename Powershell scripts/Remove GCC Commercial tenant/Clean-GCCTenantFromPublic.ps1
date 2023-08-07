27fde54bbb72f7813fe5bab8290cfd4eb49ab1aa
Eli Sagie <52936970+elSagie@users.noreply.github.com>
2023-08-07 10:20:04 +0300

    Create Clean-GCCTenantFromPublic.ps1

---
+++ Powershell scripts/Remove GCC Commercial tenant/Clean-GCCTenantFromPublic.ps1
@@ -0,0 +1,3 @@
+# This script is to invoke REST API `PUT
+# https://management.azure.com/subscriptions/<subscriptionId>/providers/Microsoft.Security/mdeOnboardings/RemoveTenantFromGccPublic?api-version=2021-10-01-preview`
+# which removed GCC Tenant from public cloud list of MDC.
